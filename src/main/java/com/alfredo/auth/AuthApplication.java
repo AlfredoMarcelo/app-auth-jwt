109d78d8979baa7a96292576647e2379b27dd64c
alfredo <alfredo.supanta.26@gmail.com>
2022-08-26 19:22:27 -0400

    se agrega controller, service UserDetail y configurationSecuirty

--- src/main/java/com/alfredo/auth/AuthApplication.java
+++ src/main/java/com/alfredo/auth/AuthApplication.java
@@ -2,6 +2,9 @@ package com.alfredo.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AuthApplication {
@@ -10,4 +13,8 @@ public class AuthApplication {
 		SpringApplication.run(AuthApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
---
+++ src/main/java/com/alfredo/auth/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.alfredo.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "hola bienvenido";
+    }
+}
